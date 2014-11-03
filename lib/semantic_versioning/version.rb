0adbc40a8199e70e655888ae6df4c4ece7ed640e
msfukui <msfukui@gmail.com>
2014-11-04 02:08:01 +0900

    #up, #up! を実装。

--- lib/semantic_versioning/version.rb
+++ lib/semantic_versioning/version.rb
@@ -4,6 +4,7 @@ module SemanticVersioning
   class Version
     SEMVER = /\A(\d+\.\d+\.\d+)\Z/.freeze
     attr_reader :major, :minor, :patch
+    attr_accessor :incremental_label
 
     def initialize(version)
       unless version =~ SEMVER
@@ -13,7 +14,40 @@ module SemanticVersioning
         )
       end
 
+      @incremental_label = :patch
+
       @major, @minor, @patch = version.split('.').map(&:to_i)
+    end
+
+    def to_s
+      [@major, @minor, @patch].join '.'
+    end
+
+    def up
+      major, minor, patch = @major, @minor, @patch
+      upgrade
+      ret = SemanticVersioning::Version.new to_s
+      @major, @minor, @patch = major, minor, patch
+      ret
+    end
+
+    def up!
+      upgrade
+      self
+    end
+
+    private
+
+    def upgrade
+      if @incremental_label == :major
+        @major += 1
+        @minor, @patch = 0, 0
+      elsif @incremental_label == :minor
+        @minor += 1
+        @patch = 0
+      else
+        @patch += 1
+      end
     end
   end
 end
